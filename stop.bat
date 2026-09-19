89997bafc1eaf159c0ab62d605d7987b31f084dd
asc3t1c <bugb0untypropentest@gmail.com>
2026-09-19 20:16:00 +0300

    Update stop.bat

--- stop.bat
+++ stop.bat
@@ -1,20 +1,19 @@
 @echo off
-TITLE nu11secur1tyAI - curtach Stopper
-echo [nu11secur1tyAI] Stopping curtach service...
+TITLE nu11secur1ty - curtach Stopper
+echo [nu11secur1ty] Stopping curtach service...
 
 :: Kill process on port 3000
 for /f "tokens=5" %%a in ('netstat -aon ^| findstr :3000') do (
     taskkill /f /pid %%a >nul 2>&1
-    echo [nu11secur1tyAI] Process (PID: %%a) terminated.
+    echo [nu11secur1ty] Process (PID: %%a) terminated.
 )
 
 :: Delete local curtach.js
 if exist curtach.js (
     del /f /q curtach.js
-    echo [nu11secur1tyAI] curtach.js cleaned up from system.
+    echo [nu11secur1ty] curtach.js cleaned up from system.
 ) else (
-    echo [nu11secur1tyAI] No local curtach.js found to delete.
+    echo [nu11secur1ty] No local curtach.js found to delete.
 )
 
 echo [nu11secur1tyAI] Cleanup complete!
-timeout /t 3 >nul
